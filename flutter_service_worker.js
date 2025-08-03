638a6e8dea7f42285eb0ec17fd2a5deeb2adbcd9
Yasin Azizipour <yazizipour@sk.local>
2025-08-03 14:39:08 +0330

    rebuild

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -142,7 +142,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e70264a76bf01715ff526b85e54cfae9
 "CNAME": "50e04d6539887bda9dd90173d2f8f93c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "72cfcdd08c54119728da3dab885e7a49",
+"flutter_bootstrap.js": "46c56720f6d0fc8e92a2e1bfdea517f7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
